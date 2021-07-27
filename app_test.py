9f7fff0dd8f67f45e2d9c01b93d89c8d79802aad
Gizar Zigangirov <77158889+nastasyafedotovna@users.noreply.github.com>
2021-07-27 15:33:42 +0300

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -3,5 +3,5 @@ from app import hello_world
 
 def test_answer():
     """test hello_world func"""
-    right_answer = "Hell0, world! 1!!!!!!!!!!!!!!!!!!!!!!!"
+    right_answer = "Hell0, world! 12!!!!!!!!!!!!!!!!!!!!!!!"
     assert right_answer == hello_world()
